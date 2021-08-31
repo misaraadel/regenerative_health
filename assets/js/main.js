8370a2781887136b683cf673890247223bb3d3df
misaraadel <41232116+misaraadel@users.noreply.github.com>
2021-08-31 14:56:54 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+$(document).ready(function () {
+    /*start the loading page */
+    window.addEventListener("load", function () {
+        const loader = document.querySelector(".loader");
+        loader.className += " hidden"; // class "loader hidden"
+    });
+    //scroll to top make fade for button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $(".scroll-top-button").addClass('active-scroll');
+        } else {
+            $(".scroll-top-button").removeClass('active-scroll');
+        }
+    });
+    //scroll to top button
+    $(".scroll-top-button").on('click', function () {
+        $('html , body').animate({
+            scrollTop: 0
+        }, 100);
+    });
+    //change navbar color on scroll
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar-fixed-top");
+            $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        });
+    });
+     //Wow intit
+     wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 200,
+        mobile: true,
+        live: false
+    });
+    wow.init();
+    //start active navbar
+    //start active navbar
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+        $('.navbar-nav').toggleClass('acitve-nav');
+        $('body').toggleClass('overflowNone')
+    });
+    $('.btn-link').click(function(){
+        $('.btn-link').toggleClass('active-arrow');
+        $('.collapse').toggleClass('collapse-show');
+    });
+    //owl header screen 
+    $('.owl-header').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 20,
+        nav:false,
+        dots: true,
+        touchDrag  :false,
+		mouseDrag  : false,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        items:1
+    });
+    //partner slider 
+    $('.owl-partner').owlCarousel({
+        rtl: true,
+        loop:false,
+        margin: 20,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+    //services slider 
+    $('.owl-services').owlCarousel({
+        rtl: true,
+        loop:false,
+        margin: 20,
+        nav:false,
+        autoplay: true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2,
+                touchDrag  : true,
+                mouseDrag  : true,
+                dots: true,
+            },
+            600:{
+                items:2,
+                touchDrag  : true,
+                mouseDrag  : true,
+                dots: true,
+            },
+            1000:{
+                items:4,
+                touchDrag  : false,
+                mouseDrag  : false,
+                dots: false,
+            }
+        }
+    });
+});
